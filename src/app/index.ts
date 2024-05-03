d80f2773abd5e6df7165aa5f6b871dc0a041afa7
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-05-04 00:53:02 +0530

    Add User

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import bodyParser from "body-parser";
+import { User } from "./user";
 
 export async function initServer() {
   const app = express();
@@ -9,14 +10,15 @@ export async function initServer() {
 
   const graphqlServer = new ApolloServer({
     typeDefs: `
+    ${User.types}
     type Query{
-        sayHello : String
+      ${User.queries}
     }
     
     `,
     resolvers: {
       Query: {
-        sayHello: () => "Hello from server",
+        ...User.resolvers.queries,
       },
     },
   });
